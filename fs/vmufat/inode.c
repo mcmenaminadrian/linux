25d549ed8a3a5295d599921d513f63c7046dde54
Adrian McMenamin <adrianmcmenamin@gmail.com>
2026-02-21 13:21:23 +0000

    use list_head

--- fs/vmufat/inode.c
+++ fs/vmufat/inode.c
@@ -742,7 +742,8 @@ static int vmufat_get_block(struct inode *inode, sector_t iblock,
 	struct buffer_head *bh_result, int create)
 {
 	struct vmufat_inode *vin;
-	struct vmufat_block *vlist, *vblk;
+	struct list_head *vlist;
+	struct vmufat_block *vblk;
 	struct super_block *sb;
 	struct memcard *vmudetails;
 	int cural;
